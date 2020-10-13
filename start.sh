d4410142d1f0ea4155840e65282ef802fe8bf9cb
Johnny Casey <jjcasey@users.noreply.github.com>
2020-10-12 20:02:31 -0500

    cleanup system specific values

--- start.sh
+++ start.sh
@@ -3,11 +3,11 @@ docker container rm apt-cacher-ng || /bin/true
 docker run -td \
 	--restart always \
 	-e DEBBASE_SYSLOG=stdout \
-	-e DEBBASE_TIMEZONE=America/Chicago \
+	-e DEBBASE_TIMEZONE=`cat /etc/timezone` \
 	--stop-signal=SIGRTMIN+3 \
 	--tmpfs /run:size=100M \
 	--tmpfs /run/lock:size=100M \
-       	-v /srv/apt-cacher-ng:/var/cache/apt-cacher-ng \
+	-v /var/cache/apt-cacher-ng:/var/cache/apt-cacher-ng \
 	-v /sys/fs/cgroup:/sys/fs/cgroup:ro \
 	-p 3142:3142 \
 	--network lan-services \
